2938ea8c3dc15021795b9ab18809a419664fafbd
Petra Söderström <101672601+Petrasoderstrom1612@users.noreply.github.com>
2026-01-24 18:36:42 +0100

    people done and pretty

--- starwars/src/pages/Films.tsx
+++ starwars/src/pages/Films.tsx
@@ -11,12 +11,12 @@ const FilmsPage = () => (
         <div className="card h-100">
           {film.image_url && <img src={film.image_url} className="card-img-top" alt={film.title} />}
           <div className="card-body d-flex flex-column">
-            <Link to={`/films/${film.id}`} className="btn btn-outline-primary mt-auto">Read More</Link>
             <h5 className="card-title">{film.title}</h5>
             <p><strong>Episode:</strong> {film.episode_id}</p>
             <p><strong>Director:</strong> {film.director}</p>
             <p><strong>Producer:</strong> {film.producer}</p>
             <p><strong>Release Date:</strong> {film.release_date}</p>
+            <Link to={`/films/${film.id}`} className="btn btn-outline-primary mt-auto">Read More</Link>
           </div>
         </div>
       </div>
--- starwars/src/pages/People.tsx
+++ starwars/src/pages/People.tsx
@@ -1,9 +1,57 @@
-const People = () => {
-  return (
-    <div>
-      people
-    </div>
-  )
-}
+import { Link } from "react-router-dom";
+import type { People } from "../services/types";
+import { ListPage } from "../components/ListPage";
+
+const People = () => (
+  <ListPage<People>
+    endpoint="/people"
+    placeholder="Search people"
+    renderItem={(person) => (
+      <div key={person.id} className="col-12 col-md-6 col-lg-4 mb-4">
+        <div className="card h-100">
+          {person.image_url && (
+            <div className="overflow-hidden" style={{ height: "600px" }}>
+              <img
+                src={person.image_url}
+                className="card-img-top w-100 h-100"
+                alt={person.name}
+                style={{ objectFit: "cover", objectPosition: "top" }}
+              />
+            </div>
+          )}
+          <div className="card-body d-flex flex-column">
+            <h5 className="card-title">{person.name}</h5>
+            <ul className="list-unstyled mb-3">
+              <li><strong>Birthdate:</strong> {person.birth_year}</li>
+              <li><strong>Eye color:</strong> {person.eye_color}</li>
+              <li><strong>Hair color:</strong> {person.hair_color}</li>
+              <li><strong>Height:</strong> {person.height} cm</li>
+              <li><strong>Mass:</strong> {person.mass} kg</li>
+              <li><strong>Skin color:</strong> {person.skin_color}</li>
+              <li><strong>Homeworld:</strong> {person.homeworld.name}</li>
+              {person.affiliations.length > 0 && (
+                <li><strong>Affiliations:</strong> {person.affiliations.join(", ")}</li>
+              )}
+            </ul>
+            <div className="mt-auto d-flex flex-column gap-2">
+              {person.wiki_link && (
+                <a
+                  href={person.wiki_link}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Wiki
+                </a>
+              )}
+              <Link to={`/people/${person.id}`} className="btn btn-outline-primary">
+                Read More
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
+    )}
+  />
+);
 
 export default People
