20787df29f60ae0d228516c85efd6c8cccb35a33
Petra Söderström <101672601+Petrasoderstrom1612@users.noreply.github.com>
2026-01-25 16:23:54 +0100

    StarshipsCommon

--- starwars/src/services/types.ts
+++ starwars/src/services/types.ts
@@ -127,4 +127,22 @@ export type Specie = SpeciesCommon & {
 };
 
 /** STARSHIPS **/
+export type StarshipsCommon = {
+  id: number;
+  name: string;
+  model: string;
+  starship_class: string;
+  manufacturer: string;
+  cost_in_credits: string;
+  length: string;
+  crew: string;
+  passengers: string;
+  max_atmosphering_speed: string;
+  MGLT: string;
+  cargo_capacity: string;
+  consumables: string;
+  created: string;
+  edited: string;
+};
+
 /** VEHICLES **/
